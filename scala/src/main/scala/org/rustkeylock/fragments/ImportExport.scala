85e43d3d9e7ae1226957088e023d1b0ebce9f175
Aston <astonbitecode@gmail.com>
2018-06-12 10:54:09 +0300

    Added lisence header

--- scala/src/main/scala/org/rustkeylock/fragments/ImportExport.scala
+++ scala/src/main/scala/org/rustkeylock/fragments/ImportExport.scala
@@ -1,3 +1,18 @@
+// Copyright 2017 astonbitecode
+// This file is part of rust-keylock password manager.
+//
+// rust-keylock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// rust-keylock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rust-keylock.  If not, see <http://www.gnu.org/licenses/>.
 package org.rustkeylock.fragments
 
 import scalafx.Includes._
@@ -161,12 +176,12 @@ class ImportExport(export: Boolean, stage: Stage, callback: Object => Unit) exte
       }
     }
 
-    private  def okButtonHandler(): Unit = {
+    private def okButtonHandler(): Unit = {
       pathTextMessage.clear()
       path = pathTextField.getText()
-      if(export) {
+      if (export) {
         if (path.isEmpty()) {
-        	logger.debug(s"Exporting to $path")
+          logger.debug(s"Exporting to $path")
           pathTextMessage.setError("Required Field")
         } else if (new File(path).isDirectory()) {
           pathTextMessage.setError("Cannot export to a directory")
@@ -178,7 +193,7 @@ class ImportExport(export: Boolean, stage: Stage, callback: Object => Unit) exte
         numberFieldMessage.clear()
 
         if (path.isEmpty()) {
-        	logger.debug(s"Importing from $path")
+          logger.debug(s"Importing from $path")
           pathTextMessage.setError("Required Field")
         } else if (new File(path).isDirectory()) {
           pathTextMessage.setError("Cannot import from a directory")
--- scala/src/main/scala/org/rustkeylock/fragments/common/PleaseWait.scala
+++ scala/src/main/scala/org/rustkeylock/fragments/common/PleaseWait.scala
@@ -1,3 +1,18 @@
+// Copyright 2017 astonbitecode
+// This file is part of rust-keylock password manager.
+//
+// rust-keylock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// rust-keylock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rust-keylock.  If not, see <http://www.gnu.org/licenses/>.
 package org.rustkeylock.fragments.common
 
 import scalafx.scene.Scene
@@ -13,7 +28,7 @@ import scalafx.scene.control.ScrollPane
 import scalafx.scene.control.ScrollPane.ScrollBarPolicy
 
 class PleaseWait(callback: Object => Unit) extends Scene {
-  
+
   root = new BorderPane() {
     style = "-fx-background: white"
     // Navigation pane
@@ -26,7 +41,7 @@ class PleaseWait(callback: Object => Unit) extends Scene {
       content = new Center
     }
   }
-  
+
   class Center() extends VBox() {
     style = "-fx-background: white"
     alignment = Pos.Center
@@ -37,4 +52,5 @@ class PleaseWait(callback: Object => Unit) extends Scene {
         style = "-fx-font-size: 24pt"
       })
   }
+
 }
