379be9f9f4795d2de04db67d605430248a019a35
Aston <astonbitecode@gmail.com>
2019-03-27 13:50:48 +0200

    Roll logs with a window.

--- rust/src/logger.rs
+++ rust/src/logger.rs
@@ -1,4 +1,3 @@
-use dirs;
 // Copyright 2017 astonbitecode
 // This file is part of rust-keylock password manager.
 //
@@ -14,23 +13,38 @@ use dirs;
 //
 // You should have received a copy of the GNU General Public License
 // along with rust-keylock.  If not, see <http://www.gnu.org/licenses/>.
+use std::{env, fs};
+
+use dirs;
 use log4rs;
-use log4rs::append::file::FileAppender;
+use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
+use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
+use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
+use log4rs::append::rolling_file::RollingFileAppender;
 use log4rs::config::{Appender, Config, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 use log::LevelFilter;
-use std::{env, fs};
 
 pub fn init() -> crate::errors::Result<()> {
     let mut logdir = dirs::home_dir().unwrap_or(env::current_dir()?);
     logdir.push(".rust-keylock/logs/");
     fs::create_dir_all(&logdir)?;
+
+    logdir.push("rust-keylock.log.{}");
+    let rollfile_pattern = logdir.to_str().unwrap_or("rust-keylock.{}.log");
+    let policy = Box::new(CompoundPolicy::new(
+        // 3 MB
+        Box::new(SizeTrigger::new(3145728)),
+        // 10 files window
+        Box::new(FixedWindowRoller::builder().build(rollfile_pattern, 10)?),
+    ));
+
+    logdir.pop();
     logdir.push("rust-keylock.log");
 
-
-    let filelogger = FileAppender::builder()
+    let filelogger = RollingFileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("[{d}][{h({l})}][{M}] - {m}{n}")))
-        .build(logdir.to_str().unwrap_or("rust-keylock.log"))?;
+        .build(logdir.to_str().unwrap_or("rust-keylock.log"), policy)?;
 
     let config = Config::builder()
         .appender(Appender::builder().build("filelogger", Box::new(filelogger)))
