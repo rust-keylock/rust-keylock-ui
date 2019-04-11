dfe279397dbf1c69dc4b77c33a81cb9128ffec4b
Aston <astonbitecode@gmail.com>
2019-04-11 12:52:56 +0300

    Implemented retrieving a token from Dropbox.

--- rust/src/callbacks.rs
+++ rust/src/callbacks.rs
@@ -149,7 +149,7 @@ fn handle_instance(jvm: &Jvm, instance: Instance) -> UserSelection {
                 debug!("set_configuration with {} elements", strings.len());
 
                 let ncc = if strings.len() == 4 {
-                    let b = match strings[3].as_ref() {
+                    let b = match strings[4].as_ref() {
                         "true" => true,
                         _ => false,
                     };
@@ -164,7 +164,13 @@ fn handle_instance(jvm: &Jvm, instance: Instance) -> UserSelection {
                                                 false)
                 };
 
-                UserSelection::UpdateConfiguration(AllConfigurations::new(ncc.unwrap(), DropboxConfiguration::default()))
+                let dbxc = if strings.len() == 4 {
+                    DropboxConfiguration::new(strings[3].clone())
+                } else {
+                    Ok(DropboxConfiguration::default())
+                };
+
+                UserSelection::UpdateConfiguration(AllConfigurations::new(ncc.unwrap(), dbxc.unwrap()))
             }
             GuiResponse::UserOptionSelected { user_option } => {
                 debug!("user_option_selected");
--- scala/src/main/scala/org/rustkeylock/fragments/EditConfiguration.scala
+++ scala/src/main/scala/org/rustkeylock/fragments/EditConfiguration.scala
@@ -172,7 +172,14 @@ case class EditConfiguration private(strings: List[String], stage: Stage, callba
 
     add(new Separator(), 0, 15, 2, 1)
     add(subtitleDropbox, 0, 16)
-    add(getTokenButton, 1, 16)
+
+    val tokenLabel = if (strings(3).isEmpty) {
+      new RklLabel("Please press the button to acquire a new authentication token")
+    } else {
+      new RklLabel("Token is acquired")
+    }
+    add(tokenLabel, 0, 17)
+    add(getTokenButton, 1, 17)
 
     add(okButton, 0, 18, 1, 1)
     add(cancelButton, 1, 18, 1, 1)
