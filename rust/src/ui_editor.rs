6e321d372ace475771638b183121aea6a8b2bd8f
Aston <astonbitecode@gmail.com>
2020-04-13 10:44:14 +0300

    Bump vo.12.0

--- rust/src/ui_editor.rs
+++ rust/src/ui_editor.rs
@@ -144,8 +144,9 @@ fn show_menu(editor: &DesktopImpl, menu: &Menu) -> errors::Result<Receiver<UserS
                 "apply",
                 &[InvocationArg::try_from("Main").unwrap()])
         }
-        &Menu::NewEntry => {
-            let empty_entry = JavaEntry::empty();
+        &Menu::NewEntry(ref entry_opt) => {
+            let entry = entry_opt.clone().unwrap_or_else(|| Entry::empty());
+            let empty_entry = JavaEntry::new(&entry);
             // In order to denote that this is a new entry, put -1 as index
             editor.jvm.invoke_to_channel(
                 &editor.show_entry,
@@ -318,16 +319,6 @@ impl JavaEntry {
             desc: entry.desc.clone(),
         }
     }
-
-    fn empty() -> JavaEntry {
-        JavaEntry {
-            name: "".to_string(),
-            url: "".to_string(),
-            user: "".to_string(),
-            pass: "".to_string(),
-            desc: "".to_string(),
-        }
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -381,7 +372,7 @@ impl JavaMenu {
             JavaMenu::ImportEntries => Menu::ImportEntries,
             JavaMenu::ShowConfiguration => Menu::ShowConfiguration,
             JavaMenu::ForceExit => Menu::ForceExit,
-            JavaMenu::NewEntry => Menu::NewEntry,
+            JavaMenu::NewEntry => Menu::NewEntry(None),
             JavaMenu::WaitForDbxTokenCallback { s } => Menu::WaitForDbxTokenCallback(s),
             JavaMenu::ShowEntry { idx } => Menu::ShowEntry(idx),
             JavaMenu::EditEntry { idx } => Menu::EditEntry(idx),
