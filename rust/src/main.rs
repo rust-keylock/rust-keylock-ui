e165be7f082df99366c630b8d651127189833dd5
Aston <astonbitecode@gmail.com>
2025-02-20 16:17:16 +0200

    Bump v0.16..0

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -27,14 +27,13 @@ use j4rs::JavaOpt;
 use log::*;
 use std::path::PathBuf;
 
-mod ui_editor;
-mod logger;
 mod callbacks;
 mod errors;
+mod logger;
+mod ui_editor;
 
 fn main() {
     logger::init().expect("Could not initialize logging ");
-
     let jh = env::var("RUST_KEYLOCK_UI_JAVA_USER_HOME")
         .map(|s| format!("-Duser.home={}", s))
         .unwrap_or("".to_string());
@@ -45,9 +44,7 @@ fn main() {
     // In a snaps environment, this is under the $SNAP/opt/j4rs.
     // In other environments, it is under the default rust-keylock location (in the home directory)
     let mut j4rs_installation_path = match env::var("RKL_J4RS_INST_DIR") {
-        Ok(path) => {
-            PathBuf::from(path)
-        }
+        Ok(path) => PathBuf::from(path),
         Err(_) => {
             let mut j4rs_installation_path_buf = rust_keylock::default_rustkeylock_location();
             j4rs_installation_path_buf.push("lib");
@@ -61,20 +58,13 @@ fn main() {
     j4rs_installation_path.push("jassets");
     let jassets_path_string = j4rs_installation_path.to_str().unwrap().to_owned();
 
-    let mut jopts: Vec<JavaOpt> = if jh.is_empty() {
-        Vec::new()
-    } else {
-        vec![JavaOpt::new(&jh)]
-    };
+    let mut jopts: Vec<JavaOpt> = if jh.is_empty() { Vec::new() } else { vec![JavaOpt::new(&jh)] };
 
     let modules_path = format!("--module-path {}", jassets_path_string);
     jopts.push(JavaOpt::new(&modules_path));
     jopts.push(JavaOpt::new("--add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml"));
 
-    let jvm_res = j4rs::JvmBuilder::new()
-        .java_opts(jopts)
-        .with_base_path(&base_path_string)
-        .build();
+    let jvm_res = j4rs::JvmBuilder::new().java_opts(jopts).with_base_path(&base_path_string).build();
 
     let jvm = jvm_res.unwrap();
     debug!("Initializing the editor");
